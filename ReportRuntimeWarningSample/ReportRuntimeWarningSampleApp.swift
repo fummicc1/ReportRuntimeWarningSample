72091ece8554723548be6d8fd735febce7fae64f
fummicc1 <fumiya.tennis1@gmail.com>
2024-11-23 12:11:17 +0900

    Initial Commit

---
+++ ReportRuntimeWarningSample/ReportRuntimeWarningSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReportRuntimeWarningSampleApp.swift
+//  ReportRuntimeWarningSample
+//
+//  Created by Fumiya Tanaka on 2024/11/23.
+//
+
+import SwiftUI
+
+@main
+struct ReportRuntimeWarningSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ReportRuntimeWarningSampleTests/ReportRuntimeWarningSampleTests.swift
@@ -0,0 +1,16 @@
+//
+//  ReportRuntimeWarningSampleTests.swift
+//  ReportRuntimeWarningSampleTests
+//
+//  Created by Fumiya Tanaka on 2024/11/23.
+//
+
+import Testing
+
+struct ReportRuntimeWarningSampleTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
